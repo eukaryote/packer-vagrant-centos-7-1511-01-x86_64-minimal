3118c81263c3808ef1f86d83ec44feb45c9592d8
eukaryote <sapientdust+github@gmail.com>
2016-01-09 11:03:15 -0800

    regenerate sshd host keys

--- scripts/sshd.sh
+++ scripts/sshd.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sed -i 's/^.*UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
+
+# remove ssh host keys and restart ssh to generate new ones
+rm -f /etc/ssh/ssh_host_*_key*
+systemctl restart sshd
